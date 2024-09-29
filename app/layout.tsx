24b563a90c2e6eb1efb36a2c8798ad28ab6e5c85
abhay <abhaym1205@gmail.com>
2024-09-29 23:29:55 +0530

    done font, shadcn, public ts setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import ConfigProvider from "@/providers/ConfigProvider";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Navbar from "@/components/custom/common/navbar";
+import { montserrat, openSans } from "../public/font.js";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${montserrat.variable} ${openSans.variable}`}>
+        <ConfigProvider>
+          <Navbar />
+          {children}
+        </ConfigProvider>
       </body>
     </html>
   );
